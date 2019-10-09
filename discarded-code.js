0872a94e35e081c082ee413df9116cffc49796c6
Heike Miersch <heike.miersch@hotmail.de>
2019-10-09 14:58:05 +0200

    created tables in js for house and senate

---
+++ discarded-code.js
@@ -0,0 +1,21 @@
+// var features = firstName + middleName + lastName + state + party + yearsInOffice + votesWithParty;
+
+// console.log(features);
+
+
+// var table = "";
+// var rows = members.length;
+// var cols = 7;
+// for (var r = 0; r < rows; r++) {
+//   table += "<tr>";
+//   for (var c = 1; c <= cols; c++) {
+//     table += "<td>" + c + "</td>";
+//   }
+//   table += "</tr>";
+// }
+// console.log(table);
+
+// if members[i] < members.length
+
+// for (i = 0; members.length; i++);
+// console.log(members[i]);
---
+++ main.js
@@ -0,0 +1,54 @@
+console.log(data);
+var members = data.results[0].members;
+console.log(members);
+
+
+// christin says: loop with vars - create table with createElement - fill it with vars - sort it with appendChild
+
+var tbody = document.getElementById("tbody")
+var members = data.results[0].members;
+var table = "";
+var rows = members.length;
+var cols = 5;
+
+for (var r = 0; r < rows; r++) {
+    var name =
+        (members[r].first_name + " ") +
+        (members[r].middle_name || "") +
+        (" " + members[r].last_name);
+
+    table +=
+        "<tr>" +
+        "<td>" +
+        name +
+        "</td>" +
+        "<td>" +
+        members[r].state +
+        "</td>" +
+        "<td>" +
+        members[r].seniority +
+        "</td>" +
+        "<td>" +
+        members[r].party +
+        "</td>" +
+        "<td>" +
+        members[r].votes_with_party_pct + " %" +
+        "</td>"
+    "</tr>";
+}
+// console.log(table);
+// console.log(tbody);
+tbody.innerHTML = table;
+console.log(tbody);
+
+
+
+
+// var rows2 = document.createElement("tr")
+// var cell1 = document.createElement("td")
+
+// cell1.innerHTML = firstName;
+// console.log(cell1)
+// rows2.appendChild(cell1)
+// console.log(rows2)
+// tbody.appendChild(rows2)
