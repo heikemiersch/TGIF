de3cf8fb9604fd944a6e2e8da2be957652e1eb49
Heike Miersch <heike.miersch@hotmail.de>
2019-10-11 17:24:11 +0200

    created least and most 10% for senate-attendance

--- statistics.js
+++ statistics.js
@@ -1,62 +1,7 @@
-console.log(data);
 var members = data.results[0].members;
-console.log(members);
-
-// this is the second table on the senate-attendance page
-
-var members = data.results[0].members;
-var table = "";
-var rows = members.length;
-
-// function name() {
-//     var name =
-//         (members[r].first_name + " ") +
-//         (members[r].middle_name || "") +
-//         (" " + members[r].last_name);
-//     return name;
-// }
 
 
-for (var r = 0; r < rows; r++) {
-    var tbody = document.getElementById("tbody2")
-    var name =
-        (members[r].first_name + " ") +
-        (members[r].middle_name || "") +
-        (" " + members[r].last_name);
-
-    table +=
-        "<tr>" +
-        "<td>" +
-        name +
-        "</td>" +
-        "<td>" +
-        members[r].missed_votes +
-        "</td>" +
-        "<td>" +
-        members[r].missed_votes_pct +
-        "</td>"
-    "</tr>";
-}
-
-
-// console.log(table);
-// console.log(tbody);
-tbody.innerHTML = table;
-console.log(tbody);
-
-// this would be the approach to create the tables in nodejs, so without strings
-
-// var rows2 = document.createElement("tr")
-// var cell1 = document.createElement("td")
-
-// cell1.innerHTML = firstName;
-// console.log(cell1)
-// rows2.appendChild(cell1)
-// console.log(rows2)
-// tbody.appendChild(rows2)
-
-
-//this is the first table on senate-attendance page
+// ↓ this is the first table on senate-attendance page ↓
 
 var tbody = document.getElementById("tbody1")
 
@@ -80,6 +25,8 @@ for (var r = 0; r < members.length; r++) {
     }
 }
 
+// ↓ the following two loops could have gone into the previous one, i know that now ↓
+
 for (var r = 0; r < members.length; r++) {
     if (members[r].party === "D") {
         statistics.democratsVotedWithParty +=
@@ -90,7 +37,6 @@ for (var r = 0; r < members.length; r++) {
 statistics.democratsVotedWithParty = statistics.democratsVotedWithParty / statistics.numberOfDemocrats;
 
 statistics.democratsVotedWithParty = (Math.floor(statistics.democratsVotedWithParty * 100) / 100);
-
 
 for (var r = 0; r < members.length; r++) {
     if (members[r].party === "R") {
@@ -104,7 +50,6 @@ statistics.republicansVotedWithParty = statistics.republicansVotedWithParty / st
 statistics.republicansVotedWithParty = (Math.floor(statistics.republicansVotedWithParty * 100) / 100);
 
 statistics.total = (statistics.numberOfDemocrats + statistics.numberOfRepublicans + statistics.numberOfIndependents);
-
 
 tbody.innerHTML +=
     "<tr>" +
@@ -148,34 +93,147 @@ tbody.innerHTML +=
     "</td>" +
     "</tr>";
 
-// this is the third table on the senate-attendance page
+// ↓ this is the second table on the senate-attendance page ↓
 
 var members = data.results[0].members;
-var table = "";
-var rows = members.length;
+var tablo = "";
 
-for (var r = 0; r < rows; r++) {
-    var tbody = document.getElementById("tbody3")
-    var name =
-        (members[r].first_name + " ") +
-        (members[r].middle_name || "") +
-        (" " + members[r].last_name);
 
-    table +=
+const arrEng = [];
+
+for (r = 0; r < members.length; r++) {
+    arrEng.push({
+        missedVotes: members[r].missed_votes,
+        name: (members[r].first_name + " ") +
+            (members[r].middle_name || "") +
+            (" " + members[r].last_name),
+        missedVotesPercentage: members[r].missed_votes_pct
+    })
+};
+
+
+
+
+// ↓ this sorts arrEng into ascending order 
+arrEng.sort((a, b) => (a.missedVotesPercentage < b.missedVotesPercentage ? 1 : -1));
+
+// ↓ this seems to be the easiest and shortest way  to do that ↓
+// list.sort((a, b) => (a.color > b.color) ? 1 : -1)
+
+// ↓ and here we have the least engaged 10% ↓
+
+var arrEngLeast = [];
+for (var r = 0; r < arrEng.length; r++) {
+    if (arrEngLeast.length < (arrEng.length * 0.1)) {
+        arrEngLeast.push(arrEng[r]);
+    }
+};
+
+
+for (var r = 0; r < arrEngLeast.length; r++) {
+
+    var leastTable = document.getElementById("least")
+    tablo +=
         "<tr>" +
         "<td>" +
-        name +
+        arrEngLeast[r].name +
         "</td>" +
         "<td>" +
-        members[r].missed_votes +
+        arrEngLeast[r].missedVotes +
         "</td>" +
         "<td>" +
-        members[r].missed_votes_pct +
-        "</td>"
-    "</tr>";
+        arrEngLeast[r].missedVotesPercentage +
+        "</td>" +
+        "</tr>";
 }
 
-// console.log(table);
+
+// console.log(tablo);
 // console.log(tbody);
-tbody3.innerHTML = table;
-console.log(tbody);
+leastTable.innerHTML = tablo;
+
+
+// this would be the approach to create the tables in nodejs, so without strings
+
+// var rows2 = document.createElement("tr")
+// var cell1 = document.createElement("td")
+
+// cell1.innerHTML = firstName;
+// console.log(cell1)
+// rows2.appendChild(cell1)
+// console.log(rows2)
+// tbody.appendChild(rows2)
+
+
+// ↓ this is the third table on the senate-attendance page ↓
+arrEng.sort((a, b) => (a.missedVotesPercentage > b.missedVotesPercentage ? 1 : -1));
+
+
+var members = data.results[0].members;
+var tabla = "";
+var rows = members.length;
+var arrEngMost = []
+
+// ↓ and here we have the most engaged 10 % ↓
+
+var arrEngMost = [];
+for (var r = 0; r < arrEng.length; r++) {
+    if (arrEngMost.length < (arrEng.length * 0.1)) {
+        arrEngMost.push(arrEng[r]);
+    }
+};
+
+console.log(arrEngMost.length)
+for (var r = 0; r < arrEngMost.length; r++) {
+    var tableMost = document.getElementById("most");
+
+    tabla +=
+        "<tr>" +
+        "<td>" +
+        arrEngMost[r].name +
+
+        "</td>" +
+        "<td>" +
+        arrEngMost[r].missedVotes +
+        "</td>" +
+        "<td>" +
+        arrEngMost[r].missedVotesPercentage +
+        "</td>" +
+        "</tr>";
+
+}
+console.log(tabla)
+
+
+// console.log(tbody);
+tableMost.innerHTML = tabla;
+
+
+
+
+
+// ↓ this was my attempt to sort out the 10% ↓
+
+// var highestTen = [];
+
+// function highestTenPercent(a, b) {
+
+//     for (var r = 0; r < arrEng.length; r++) {
+//         if (highestTen < (arrEng.length * 0.1)) {
+//             highestTen.push(Math.max(arrEng.missed_votes_pct))
+//         }
+//     }
+//     highestTenPercent(highestTen)
+// };
+// console.log(highestTen);
+
+// console.log(highestTen.push(Math.max(arrEng.missed_votes_pct)));
+
+// 216 this is where i want to push the results of the function
+
+// 218 the function which i guess cannot work with just a and b
+// 220 loop through arrEng.length
+// 221 as long as highestTen is smaller than 10& of arrEng.length
+// 222 push result of loop (the max number than was found) into var highestTen
+// 225 call function
+// 227 console.log it to check
