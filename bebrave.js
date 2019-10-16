d232ab54374d63dc64ede1266bfbcf5a65969b9d
Heike Miersch <heike.miersch@hotmail.de>
2019-10-16 21:33:28 +0200

    quick attempt to create checkboxes

--- bebrave.js
+++ bebrave.js
@@ -1,106 +1,24 @@
-// ↓ this is the second table on the loyalty pages ↓
+// Get checked box values and put them into an array.
+// Use that array to filter the list of members to pass to your
+// function to create the table.
+// Call this code whenever a checkbox is changed, i.e., use an onchanged event listener.
 
-var table5 = "";
+document.getElementById("click").addEventListener("click", createChbox);
 
-const arrLoy = [];
+// element.addEventListener("click", createChbox);
 
-if (document.title == "Senate Party Loyalty" || document.title == "House Party Loyalty") {
-    createLoyList();
-
-
-    function createLoyList() {
-        for (r = 0; r < members.length; r++) {
-            if (members[r].total_votes != null && members[r].votes_with_party_pct !== "undefined") {
-                arrLoy.push({
-                    totalPartyVotes: ((members[r].total_votes - members[r].missed_votes) / 100) * members[r].votes_with_party_pct,
-                    name: (members[r].first_name + " ") +
-                        (members[r].middle_name || "") +
-                        (" " + members[r].last_name),
-                    partyVotesPercentage: members[r].votes_with_party_pct
-                })
-            }
-        };
-
-        // ↓ this sorts arrLoy into ascending order 
-        arrLoy.sort((a, b) => (a.partyVotesPercentage < b.partyVotesPercentage ? 1 : -1));
-        // ↓ this seems to be the easiest and shortest way  to do that ↓
-        // list.sort((a, b) => (a.color > b.color) ? 1 : -1)
-
-        var arrLoyLeast = [];
-        for (var r = 0; r < arrLoy.length; r++) {
-            if (arrLoyLeast.length < (arrLoy.length * 0.1)) {
-                arrLoyLeast.push(arrLoy[r]);
-            }
-        };
-        createLoyLeastTable(arrLoyLeast);
-
-
-        arrLoy.sort((a, b) => (a.partyVotesPercentage > b.partyVotesPercentage ? 1 : -1));
-
-
-        var arrLoyMost = [];
-        for (var r = 0; r < arrEng.length; r++) {
-            if (arrLoyMost.length < (arrLoy.length * 0.1)) {
-                arrLoyMost.push(arrLoy[r]);
-            }
-        };
-        createLoyMostTable(arrLoyMost);
-    }
-
-    function createLoyLeastTable(arr) {
-        var leastTable = document.getElementById("leastLoyal")
-        for (var r = 0; r < arrLoyLeast.length; r++) {
-            table5 +=
-                "<tr>" +
-                "<td>" +
-                arr[r].name +
-                "</td>" +
-                "<td>" +
-                arr[r].totalPartyVotes +
-                "</td>" +
-                "<td>" +
-                arr[r].partyVotesPercentage +
-                "</td>" +
-                "</tr>";
+function createChbox() {
+    var arrRep = [];
+    var arrDem = [];
+    var arrInd = [];
+    for (var r = 0; r > members[r].length; r++) {
+        if (members[r].party === D) {
+            arrDem.push;
+        } else if (members[r].party === R) {
+            arrRep.push;
+        } else if (members[r].party === I) {
+            arrInd.push;
         }
-        leastTable.innerHTML = table5;
     }
-
-    // ↓ this is the third table on the Loyality page ↓
-    // arrLoy.sort((a, b) => (a.partyVotesPercentage > b.partyVotesPercentage ? 1 : -1));
-
-    // var members = data.results[0].members;
-    // var table4 = "";
-    // var rows = members.length;
-    // var arrLoyMost = []
-
-    // // ↓ and here we have the most engaged 10 % ↓
-
-    // var arrLoyMost = [];
-    // for (var r = 0; r < arrLoy.length; r++) {
-    //     if (arrLoyMost.length < (arrLoy.length * 0.1)) {
-    //         arrLoyMost.push(arrLoy[r]);
-    //     }
-    // };
-
-
-    function createLoyMostTable(arrLoyMost) {
-        var tableMost = document.getElementById("mostLoyal");
-        var table4 = "";
-        for (var r = 0; r < arrLoyMost.length; r++) {
-
-            table4 +=
-                "<tr>" +
-                "<td>" +
-                arrLoyMost[r].name +
-                "</td>" +
-                "<td>" +
-                arrLoyMost[r].totalPartyVotes +
-                "</td>" +
-                "<td>" +
-                arrLoyMost[r].partyVotesPercentage +
-                "</td>" +
-                "</tr>";
-        }
-        tableMost.innerHTML = table4;
-    }
+};
+createChbox();
